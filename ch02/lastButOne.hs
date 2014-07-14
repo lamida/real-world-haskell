53b391f003c6f8df803d4053c56ca12422343ba5
Jon Kartago Lamida <me@lamida.net>
2014-07-15 02:25:58 +0700

    add ch02

---
+++ ch02/lastButOne.hs
@@ -0,0 +1,3 @@
+-- exercise 
+lastButOne :: [a] -> a
+lastButOne xs = last (take (length xs - 1) xs)
---
+++ ch02/myDrop.hs
@@ -0,0 +1,5 @@
+-- file: ch03/myDrop.hs
+myDrop :: Int -> [a] -> [a]
+myDrop n xs = if n <= 0 || null xs
+            then xs
+            else myDrop (n - 1) (tail xs)
